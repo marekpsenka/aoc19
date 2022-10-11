638cb1558ea4b9924000f8fcd30c5d90d744bafa
Marek Pšenka <psenka@edhouse.cz>
2022-10-11 22:35:35 +0200

    Day 05 complete

--- src/bin/day05b.rs
+++ src/bin/day05b.rs
@@ -4,7 +4,7 @@ fn main() {
     let mut line = String::new();
     _ = std::io::stdin().read_line(&mut line);
     let code = intcode::parse_code(&line).unwrap();
-    let mut computer = intcode::Computer::new(&code, vec![1]);
+    let mut computer = intcode::Computer::new(&code, vec![5]);
     computer.run().unwrap();
     for i in computer.output() {
         println!("{i}");
--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,22 @@ pub mod intcode {
         },
         Input,
         Output(ParameterMode),
+        JumpIfTrue {
+            fst_mode: ParameterMode,
+            snd_mode: ParameterMode,
+        },
+        JumpIfFalse {
+            fst_mode: ParameterMode,
+            snd_mode: ParameterMode,
+        },
+        LessThan {
+            fst_mode: ParameterMode,
+            snd_mode: ParameterMode,
+        },
+        Equals {
+            fst_mode: ParameterMode,
+            snd_mode: ParameterMode,
+        },
         Terminate,
     }
 
@@ -36,22 +52,92 @@ pub mod intcode {
     }
 
     impl Operation {
+        fn from_single_digit_integer(integer: i32) -> Result<Operation, ParseOperationError> {
+            match integer {
+                1 => Ok(Operation::Add {
+                    fst_mode: ParameterMode::Position,
+                    snd_mode: ParameterMode::Position,
+                }),
+                2 => Ok(Operation::Multiply {
+                    fst_mode: ParameterMode::Position,
+                    snd_mode: ParameterMode::Position,
+                }),
+                3 => Ok(Operation::Input),
+                4 => Ok(Operation::Output(ParameterMode::Position)),
+                5 => Ok(Operation::JumpIfTrue {
+                    fst_mode: ParameterMode::Position,
+                    snd_mode: ParameterMode::Position,
+                }),
+                6 => Ok(Operation::JumpIfFalse {
+                    fst_mode: ParameterMode::Position,
+                    snd_mode: ParameterMode::Position,
+                }),
+                7 => Ok(Operation::LessThan {
+                    fst_mode: ParameterMode::Position,
+                    snd_mode: ParameterMode::Position,
+                }),
+                8 => Ok(Operation::Equals {
+                    fst_mode: ParameterMode::Position,
+                    snd_mode: ParameterMode::Position,
+                }),
+                _ => Err(ParseOperationError),
+            }
+        }
+
+        fn from_three_digit_integer(integer: i32) -> Result<Operation, ParseOperationError> {
+            match integer {
+                101 => Ok(Operation::Add {
+                    fst_mode: ParameterMode::Immediate,
+                    snd_mode: ParameterMode::Position,
+                }),
+                102 => Ok(Operation::Multiply {
+                    fst_mode: ParameterMode::Immediate,
+                    snd_mode: ParameterMode::Position,
+                }),
+                104 => Ok(Operation::Output(ParameterMode::Immediate)),
+                105 => Ok(Operation::JumpIfTrue {
+                    fst_mode: ParameterMode::Immediate,
+                    snd_mode: ParameterMode::Position,
+                }),
+                106 => Ok(Operation::JumpIfFalse {
+                    fst_mode: ParameterMode::Immediate,
+                    snd_mode: ParameterMode::Position,
+                }),
+                107 => Ok(Operation::LessThan {
+                    fst_mode: ParameterMode::Immediate,
+                    snd_mode: ParameterMode::Position,
+                }),
+                108 => Ok(Operation::Equals {
+                    fst_mode: ParameterMode::Immediate,
+                    snd_mode: ParameterMode::Position,
+                }),
+                _ => Err(ParseOperationError),
+            }
+        }
+
+        fn from_four_digit_integer(digits: &[char]) -> Result<Operation, ParseOperationError> {
+            format!("{}{}", digits[2], digits[3])
+                .parse::<i32>()
+                .map_err(|_| ParseOperationError)
+                .and_then(|i| ParameterMode::from_char(digits[1]).map(|mode| (i, mode)))
+                .and_then(|(i, fst_mode)| {
+                    ParameterMode::from_char(digits[0]).map(|mode| (i, fst_mode, mode))
+                })
+                .and_then(|(i, fst_mode, snd_mode)| match i {
+                    1 => Ok(Operation::Add { fst_mode, snd_mode }),
+                    2 => Ok(Operation::Multiply { fst_mode, snd_mode }),
+                    5 => Ok(Operation::JumpIfTrue { fst_mode, snd_mode }),
+                    6 => Ok(Operation::JumpIfFalse { fst_mode, snd_mode }),
+                    7 => Ok(Operation::LessThan { fst_mode, snd_mode }),
+                    8 => Ok(Operation::Equals { fst_mode, snd_mode }),
+                    _ => Err(ParseOperationError),
+                })
+        }
+
         fn from_integer(integer: i32) -> Result<Operation, ParseOperationError> {
             let digits: Vec<char> = integer.to_string().chars().collect();
             match digits.len() {
-                1 => match integer {
-                        1 => Ok(Operation::Add {
-                            fst_mode: ParameterMode::Position,
-                            snd_mode: ParameterMode::Position,
-                        }),
-                        2 => Ok(Operation::Multiply {
-                            fst_mode: ParameterMode::Position,
-                            snd_mode: ParameterMode::Position,
-                        }),
-                        3 => Ok(Operation::Input),
-                        4 => Ok(Operation::Output(ParameterMode::Position)),
-                        _ => Err(ParseOperationError),
-                    },
+                1 => Operation::from_single_digit_integer(integer),
                 2 => {
                     if integer != 99 {
                         Err(ParseOperationError)
@@ -59,30 +145,8 @@ pub mod intcode {
                         Ok(Operation::Terminate)
                     }
                 }
-                3 => match integer {
-                    101 => Ok(Operation::Add {
-                        fst_mode: ParameterMode::Immediate,
-                        snd_mode: ParameterMode::Position,
-                    }),
-                    102 => Ok(Operation::Multiply {
-                        fst_mode: ParameterMode::Immediate,
-                        snd_mode: ParameterMode::Position,
-                    }),
-                    104 => Ok(Operation::Output(ParameterMode::Immediate)),
-                    _ => Err(ParseOperationError)
-                },
-                4 => format!("{}{}", digits[2], digits[3])
-                    .parse::<i32>()
-                    .map_err(|_| ParseOperationError)
-                    .and_then(|i| ParameterMode::from_char(digits[1]).map(|mode| (i, mode)))
-                    .and_then(|(i, fst_mode)| {
-                        ParameterMode::from_char(digits[0]).map(|mode| (i, fst_mode, mode))
-                    })
-                    .and_then(|(i, fst_mode, snd_mode)| match i {
-                        1 => Ok(Operation::Add { fst_mode, snd_mode }),
-                        2 => Ok(Operation::Multiply { fst_mode, snd_mode }),
-                        _ => Err(ParseOperationError),
-                    }),
+                3 => Operation::from_three_digit_integer(integer),
+                4 => Operation::from_four_digit_integer(&digits),
                 _ => Err(ParseOperationError),
             }
         }
@@ -154,6 +218,96 @@ pub mod intcode {
             Ok(())
         }
 
+        fn exec_jump_if_true(
+            &mut self,
+            fst_mode: ParameterMode,
+            snd_mode: ParameterMode,
+        ) -> Result<(), ExecError> {
+            let value = match fst_mode {
+                ParameterMode::Position => self.memory[self.memory[self.ptr + 1] as usize],
+                ParameterMode::Immediate => self.memory[self.ptr + 1],
+            };
+
+            if value != 0 {
+                let destination = match snd_mode {
+                    ParameterMode::Position => self.memory[self.memory[self.ptr + 2] as usize],
+                    ParameterMode::Immediate => self.memory[self.ptr + 2],
+                };
+
+                self.ptr = destination as usize;
+                Ok(())
+            } else {
+                self.ptr += 3;
+                Ok(())
+            }
+        }
+
+        fn exec_jump_if_false(
+            &mut self,
+            fst_mode: ParameterMode,
+            snd_mode: ParameterMode,
+        ) -> Result<(), ExecError> {
+            let value = match fst_mode {
+                ParameterMode::Position => self.memory[self.memory[self.ptr + 1] as usize],
+                ParameterMode::Immediate => self.memory[self.ptr + 1],
+            };
+
+            if value == 0 {
+                let destination = match snd_mode {
+                    ParameterMode::Position => self.memory[self.memory[self.ptr + 2] as usize],
+                    ParameterMode::Immediate => self.memory[self.ptr + 2],
+                };
+
+                self.ptr = destination as usize;
+                Ok(())
+            } else {
+                self.ptr += 3;
+                Ok(())
+            }
+        }
+
+        fn exec_less_than(
+            &mut self,
+            fst_mode: ParameterMode,
+            snd_mode: ParameterMode,
+        ) -> Result<(), ExecError> {
+            let left_operand = match fst_mode {
+                ParameterMode::Position => self.memory[self.memory[self.ptr + 1] as usize],
+                ParameterMode::Immediate => self.memory[self.ptr + 1],
+            };
+
+            let right_operand = match snd_mode {
+                ParameterMode::Position => self.memory[self.memory[self.ptr + 2] as usize],
+                ParameterMode::Immediate => self.memory[self.ptr + 2],
+            };
+
+            let dest = self.memory[self.ptr + 3] as usize;
+            self.memory[dest] = if left_operand < right_operand { 1 } else { 0 };
+            self.ptr += 4;
+            Ok(())
+        }
+
+        fn exec_equals(
+            &mut self,
+            fst_mode: ParameterMode,
+            snd_mode: ParameterMode,
+        ) -> Result<(), ExecError> {
+            let left_operand = match fst_mode {
+                ParameterMode::Position => self.memory[self.memory[self.ptr + 1] as usize],
+                ParameterMode::Immediate => self.memory[self.ptr + 1],
+            };
+
+            let right_operand = match snd_mode {
+                ParameterMode::Position => self.memory[self.memory[self.ptr + 2] as usize],
+                ParameterMode::Immediate => self.memory[self.ptr + 2],
+            };
+
+            let dest = self.memory[self.ptr + 3] as usize;
+            self.memory[dest] = if left_operand == right_operand { 1 } else { 0 };
+            self.ptr += 4;
+            Ok(())
+        }
+
         fn exec_op(&mut self, op: Operation) -> Result<(), ExecError> {
             match op {
                 Operation::Add { fst_mode, snd_mode } => self.exec_add(fst_mode, snd_mode),
@@ -182,6 +336,18 @@ pub mod intcode {
                         Ok(())
                     }
                 },
+                Operation::JumpIfTrue { fst_mode, snd_mode } => {
+                    self.exec_jump_if_true(fst_mode, snd_mode)
+                }
+                Operation::JumpIfFalse { fst_mode, snd_mode } => {
+                    self.exec_jump_if_false(fst_mode, snd_mode)
+                }
+                Operation::LessThan { fst_mode, snd_mode } => {
+                    self.exec_less_than(fst_mode, snd_mode)
+                }
+                Operation::Equals { fst_mode, snd_mode } => {
+                    self.exec_equals(fst_mode, snd_mode)
+                }
                 Operation::Terminate => Err(ExecError),
             }
         }
